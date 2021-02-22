573b20270d08a23ff1dc7853830d6944c9000626
zjp <360943749@qq.com>
2021-02-22 18:32:02 +0800

    2.22

--- app/Exports/CleanExport.php
+++ app/Exports/CleanExport.php
@@ -4,19 +4,35 @@ namespace App\Exports;
 
 use App\Model\User;
 use App\Model\GetClean;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
-class CleanExport implements  ShouldAutoSize, WithHeadings, FromQuery
+class CleanExport implements FromCollection,  ShouldAutoSize, WithHeadings
 {
+    use Exportable;
+    public $startTime;
+    public $stopTime;
 
-
-    public function query()
+    public function __construct($startTime, $stopTime)		// 导入外部查询参数
     {
-        return GetClean::query()->whereIn('permission',$this->userPermission());                
+        $this->startTime = $startTime;
+        $this->stopTime = $stopTime;
+
+    }
+    public function collection()
+    {
+
+        return GetClean::query()
+            ->whereDate('created_at','>=',$this->startTime)
+            ->whereDate('created_at','<=',$this->stopTime)
+            ->whereIn('permission',$this->userPermission())
+
+            ->cursor(); // ← 重要的一点
+
     }
 
     public function headings(): array
